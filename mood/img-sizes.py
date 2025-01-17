5cbfd8ef24ee94ee1a9d4432f07743c0b9e16e12
Guilherme Garcia <gui.garcia67@gmail.com>
2025-01-17 18:22:55 -0300

    Add new things to mood page

--- mood/img-sizes.py
+++ mood/img-sizes.py
@@ -13,6 +13,39 @@ def get_target_height(filename, target_width):
 
 
 files = [
+    "./A-Grande-Onda-de-Kanagawa.webp",
+    "./akira-tape-br-distrib.webp",
+    "./carlos-bakery-pistache.webp",
+    "./colonias-japonesas-30-teatro.webp",
+    "./rotating-burgor-mp4-res.mp4",
+    "./cow-red.webp",
+    "./cyberpunk-tokyo-dave-murray.webp",
+    "./era-isso-mesmo-que-eu-procurava-bleach.webp",
+    "./hercilio-luz-fln.webp",
+    "./steve-harris-fucking-it-up-mp4-res.mp4",
+    "./hidden-girl-kitkat.webp",
+    "./igreja-sto-antonio-lisboa.webp",
+    "./LEPRECHAUN-sto-antonio-lisboa.webp",
+    "./maiden-hands.webp",
+    "./the-beautiful-hercilio-luz-mp4-res.mp4",
+    "./mamba-wota.webp",
+    "./not-sure-painting-reminds-of-samurai-shamploo.webp",
+    "./old-asf-kimono.webp",
+    "./view-of-the-beiramar-mp4-res.mp4",
+    "./poa-ccmq-natal.webp",
+    "./slam-dunk-manga-34-90.webp",
+    "./push.webp",
+    "./rosa-e-azul-1881-renoir.webp",
+    "./samurai-gear.webp",
+    "./sculpted-lion-santo-antonio-lisboa.webp",
+    "./what-is-the-absurd-human-nature-mp4-res.mp4",
+    "./potato-balls.webp",
+    "./some-crossing-in-fln.webp",
+    "./some-kickass-painting-angels-goth.webp",
+    "./tea-coffee-options-machine.webp",
+    "./trensurb-bench.webp",
+    "./two-juice.webp",
+    "./viados-atraves-grade-zoo.webp",
     "./cooking-soul-whateva-vol-2.webp",
     "./willhelm-screamer.webp",
     "./arrombadores.mp4",
@@ -44,16 +77,15 @@ files = [
     "./dizzy.webp",
     "./jupy-fades.mp4",
     "./structure-and-interpretation.webp",
-    "./sun-go-down.webp"   
+    "./sun-go-down.webp",
 ]
 
 img_extensions = (".png", ".jpg", ".jpeg", ".bmp", ".gif", ".webp")
 video_extensions = (".mp4",)
-target_width = 300 / 2 # smallest supported screen size (320px - 10px of padding on each side of the screen)
-columns = [
-    "<div class='left'>",
-    "<div class='right'>"
-]
+target_width = (
+    300 / 2
+)  # smallest supported screen size (320px - 10px of padding on each side of the screen)
+columns = ["<div class='left'>", "<div class='right'>"]
 
 for idx, filename in enumerate(files):
     loading = "lazy"
@@ -80,16 +112,15 @@ for idx, filename in enumerate(files):
         cap.release()
 
         min_height = get_target_height(video_poster, target_width)
-        media = (
-            f"""
+        media = f"""
 <video loop controls preload="none" style="min-height: {min_height}" poster="{video_poster}">
     <source src="{filename}" type="video/mp4">
 </video>"""
-        )
-    
+
     columns[idx % 2] += f"{media}\n"
 
-print("""
+print(
+    """
 <!DOCTYPE html>
 <html lang="en">
 
@@ -154,14 +185,17 @@ print("""
         <h1>guites' mood board</h1>
         <p>doing wall sits because I've got weak knees and strong ambitions</p>
         <section id="gallery">
-""")
+"""
+)
 for column in columns:
     print(f"{column}</div>\n")
 
-print("""
+print(
+    """
       </section>
     </main>
 </body>
 
 
-</html>""")
+</html>"""
+)
